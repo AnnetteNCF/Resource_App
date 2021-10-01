7895e6a83820562714816de8fee4019a36860ff0
annette <annette@champria.gg>
2021-09-30 18:08:21 -0700

    add script page

---
+++ script.js
@@ -0,0 +1,17 @@
+// const menuContainer = document.querySelector('#menu');
+
+// function menuDisplay() {
+//     menuContainer.classList.toggle('active');
+//     console.log('I AM A BUTTON!');
+// }
+
+
+const menuContainer = document.querySelector('#menu');
+
+function menuDisplay() {
+    menuContainer.classList.toggle('active');
+    topLine.classList.toggle('rotate-top');
+    console.log('I AM A MENU BUTTON!');
+  }
+
+//   menuContainer.addEventListener('click', menuContainer);
